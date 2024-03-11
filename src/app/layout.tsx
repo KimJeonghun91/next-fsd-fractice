be2751dc6cbc3237b30d24b1d1fe2cd6a26d7684
righthot <righthot@everon.co.kr>
2024-03-11 10:25:25 +0900

    Support for defaultProps will be removed ... 경고 무시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
+import React, { useEffect } from 'react';
 import type { Metadata, Viewport } from 'next';
 import { Noto_Sans_KR } from "next/font/google";
 import "@/shared/styles/globals.css";
 
 const defaultFont = Noto_Sans_KR({ subsets: ["latin"] });
+
+// ! Support for defaultProps will be removed ... 경고 무시
+// TODO: defaultProps 제거
+const error = console.error;
+console.error = (...args: any) => {
+  if (/defaultProps/.test(args[0])) return;
+  error(...args);
+};
 
 export const metadata: Metadata = {
   title: "Create Custom Next App",
