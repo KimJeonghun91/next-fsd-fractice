bc205a30a04a1427656128c1c75f8f2b474f1305
righthot <righthot@everon.co.kr>
2024-03-12 18:35:49 +0900

    feat: drawer menu list 분리

---
+++ src/widgets/LayoutDrawer/ui/drawer-menu-list.tsx
@@ -0,0 +1,43 @@
+
+import ListItem from 'my-mui/material/ListItem';
+import ListItemButton from 'my-mui/material/ListItemButton';
+import ListItemIcon from 'my-mui/material/ListItemIcon';
+import ListItemText from 'my-mui/material/ListItemText';
+import List from 'my-mui/material/List';
+import { DrawerMenuItem } from '../model/types';
+
+interface Props {
+    open: boolean;
+    menuList: DrawerMenuItem[];
+}
+
+const DrawerMenuList: React.FC<Props> = ({ open, menuList }) => {
+    return (
+        <List>
+            {menuList.map((item, index) => (
+                <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+                    <ListItemButton
+                        sx={{
+                            minHeight: 48,
+                            justifyContent: open ? 'initial' : 'center',
+                            px: 2.5,
+                        }}
+                    >
+                        <ListItemIcon
+                            sx={{
+                                minWidth: 0,
+                                mr: open ? 2 : 'auto',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            {item.labelIcon}
+                        </ListItemIcon>
+                        <ListItemText primary={item.title} sx={{ opacity: open ? 1 : 0 }} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+        </List>
+    )
+};
+
+export default DrawerMenuList;
